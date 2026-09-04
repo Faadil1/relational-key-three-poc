a2b8e43978e3329cfa2c99d5a3821a33ea03c2e0
Faadil1 <boussarifaadil@yahoo.com>
2026-09-04 14:59:21 -0400

    feat(v2.3): lazy-load Wave 001 families

--- v2-app/src/sceneRegistry.js
+++ v2-app/src/sceneRegistry.js
@@ -4,4 +4,7 @@ export const sceneComponents = Object.freeze({
   'anamorphosis-paris': lazy(() => import('./sceneEntries/AnamorphosisEntry.jsx')),
   'coupler-virginia': lazy(() => import('./sceneEntries/CouplerEntry.jsx')),
   'ombak-bali': lazy(() => import('./sceneEntries/OmbakEntry.jsx')),
+  'kento-japan': lazy(() => import('./sceneEntries/KentoEntry.jsx')),
+  'stereoscopy-uk': lazy(() => import('./sceneEntries/StereoscopyEntry.jsx')),
+  'signal-nigeria': lazy(() => import('./sceneEntries/SignalEntry.jsx')),
 });
