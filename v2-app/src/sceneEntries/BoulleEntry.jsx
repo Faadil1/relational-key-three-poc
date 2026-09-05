4cadc373f511aa949adfd9f0886ab43a5a80850a
Faadil1 <boussarifaadil@yahoo.com>
2026-09-05 09:34:08 -0400

    feat(v2): add Boulle lazy entry

---
+++ v2-app/src/sceneEntries/BoulleEntry.jsx
@@ -0,0 +1,3 @@
+import { FamilyCanvas } from '../FamilyCanvas.jsx';
+import { BoulleScene } from '../scenes/BoulleScene.jsx';
+export default function BoulleEntry(props){return <FamilyCanvas sceneId="boulle-france"><BoulleScene {...props}/></FamilyCanvas>}
