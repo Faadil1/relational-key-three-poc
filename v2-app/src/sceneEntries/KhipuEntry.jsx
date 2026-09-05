75614910b5e7f2d0b6e06cfc8924c9e9449c8de9
Faadil1 <boussarifaadil@yahoo.com>
2026-09-05 09:34:15 -0400

    feat(v2): add Khipu lazy entry

---
+++ v2-app/src/sceneEntries/KhipuEntry.jsx
@@ -0,0 +1,3 @@
+import { FamilyCanvas } from '../FamilyCanvas.jsx';
+import { KhipuScene } from '../scenes/KhipuScene.jsx';
+export default function KhipuEntry(props){return <FamilyCanvas sceneId="khipu-peru"><KhipuScene {...props}/></FamilyCanvas>}
