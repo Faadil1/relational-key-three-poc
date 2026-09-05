a55dbb229a7ce0e93d6c6ec6ab8d1d1830c48013
Faadil1 <boussarifaadil@yahoo.com>
2026-09-05 09:34:25 -0400

    feat(v2): add Mate Bombilla lazy entry

---
+++ v2-app/src/sceneEntries/MateBombillaEntry.jsx
@@ -0,0 +1,3 @@
+import { FamilyCanvas } from '../FamilyCanvas.jsx';
+import { MateBombillaScene } from '../scenes/MateBombillaScene.jsx';
+export default function MateBombillaEntry(props){return <FamilyCanvas sceneId="mate-bombilla-argentina"><MateBombillaScene {...props}/></FamilyCanvas>}
